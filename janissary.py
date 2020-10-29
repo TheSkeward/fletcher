7dc0ad267c9a860d9a10ab0d59a06eba46f10307
Novalinium <nova@noblejury.com>
2020-10-29 11:05:01 -0400

    Pin ugh

--- janissary.py
+++ janissary.py
@@ -1379,7 +1379,7 @@ async def unpin_message_function(message, client, args):
             or ch.is_admin(message, user=args[1])["channel"]
         ):
             if not message.channel.permissions_for(
-                ch.guild.get_member(client.user.id)
+                message.guild.get_member(client.user.id)
             ).manage_messages:
                 return await messagefuncs.sendWrappedMessage(
                     "I don't have permission to unpin messages in this channel, presumably due to misconfiguration. Please ask an admin to grant me the Manage Messages permission and try again.",
@@ -1420,7 +1420,7 @@ async def pin_message_function(message, client, args):
             or ch.is_admin(message, user=args[1])["channel"]
         ):
             if not message.channel.permissions_for(
-                ch.guild.get_member(client.user.id)
+                message.guild.get_member(client.user.id)
             ).manage_messages:
                 return await messagefuncs.sendWrappedMessage(
                     "I don't have permission to pin messages in this channel, presumably due to misconfiguration. Please ask an admin to grant me the Manage Messages permission and try again.",
