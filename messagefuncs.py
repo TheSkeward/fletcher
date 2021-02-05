a533aed96925fbe7b07ac03ba4f3ac9e5824460c
Novalinium <nova@noblejury.com>
2021-02-05 14:30:46 -0500

    pfp command

--- messagefuncs.py
+++ messagefuncs.py
@@ -850,6 +850,16 @@ def autoload(ch):
     )
     ch.add_command(
         {
+            "trigger": ["!pfp", "!avatar"],
+            "function": lambda message, client, args: (message.mentions[0] if len(message.mentions) else message.author).avatar_url,
+            "async": False,
+            "args_num": 0,
+            "args_name": [],
+            "description": "Return user's profile picture",
+        }
+    )
+    ch.add_command(
+        {
             "trigger": ["!message"],
             "function": messagelink_function,
             "async": True,
