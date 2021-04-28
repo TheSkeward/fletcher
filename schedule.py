004f40eb9d4507068b08c9612c9a4ba1cd7c04ac
Novalinium <nova@noblejury.com>
2021-04-27 20:29:14 -0400

    DMCHannel reminder fix

--- schedule.py
+++ schedule.py
@@ -288,7 +288,7 @@ async def table_exec_function():
 
 async def reminder_function(message, client, args):
     try:
-        if 378641129916203019 in [member.id for member in message.channel.members]:
+        if type(channel) is discord.DMChannel and 378641129916203019 in [member.id for member in message.channel.members]:
             return
         global conn
         cur = conn.cursor()
