b8960e93ee79bd978c340a10b071f4a59a007a84
Novalinium <nova@noblejury.com>
2021-12-27 20:29:29 -0500

    ignore bad channels

--- utils/tocgen.py
+++ utils/tocgen.py
@@ -1,10 +1,11 @@
 import configparser
 import json
 import discord
+import traceback
 import os
 import sys
 
-FLETCHER_CONFIG = os.getenv('FLETCHER_CONFIG', './.fletcherrc')
+FLETCHER_CONFIG = os.getenv("FLETCHER_CONFIG", "./.fletcherrc")
 
 config = configparser.ConfigParser()
 config.read(FLETCHER_CONFIG)
@@ -14,7 +15,8 @@ intents.members = True
 client = discord.Client(intents=intents, chunk_guilds_at_startup=False)
 
 # token from https://discordapp.com/developers
-token = config['discord']['botToken']
+token = config["discord"]["botToken"]
+
 
 @client.event
 async def on_ready():
@@ -30,15 +32,27 @@ async def on_ready():
         entries = []
         async for message in channel.history(limit=None, oldest_first=True):
             if message.raw_channel_mentions:
-                entries.append({
-                    "name": discord.utils.get(channels, id=message.raw_channel_mentions[0]).name,
-                    "created": str(message.created_at),
-                    "author": str(message.author),
-                    "content": message.clean_content.replace(f'<#{message.raw_channel_mentions[0]}>', f'#{discord.utils.get(channels, id=message.raw_channel_mentions[0]).name}'),
-                    "jump_url": message.jump_url
-                    })
+                channel = discord.utils.get(
+                    channels, id=message.raw_channel_mentions[0]
+                )
+                if channel:
+                    entries.append(
+                        {
+                            "name": channel.name,
+                            "created": str(message.created_at),
+                            "author": str(message.author),
+                            "content": message.clean_content.replace(
+                                f"<#{message.raw_channel_mentions[0]}>",
+                                f"#{channel.name}",
+                            ),
+                            "jump_url": message.jump_url,
+                        }
+                    )
         print(json.dumps(entries))
     except Exception as e:
         print(e)
+        print(traceback.print_tb(e.traceback()))
     await client.close()
+
+
 client.run(token)
