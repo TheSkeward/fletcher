796287f4e3544e8e36d6e915fb8b2f4f8e480394
Novalinium <nova@noblejury.com>
2022-03-03 00:58:21 -0500

    yt_dlp

--- swag.py
+++ swag.py
@@ -36,7 +36,7 @@ from datetime import datetime, timedelta
 from markdownify import markdownify
 from functools import partial, lru_cache
 import periodictable
-from yt_dlp import YoutubeDL
+import yt_dlp
 from typing import List
 
 # Super Waifu Animated Girlfriend
@@ -1782,17 +1782,12 @@ async def tiktok_function(message, _, args):
         input_image_blob = None
         file_name = None
         try:
-            import yt_dlp.utils
-
-            yt_dlp.utils.std_headers[
-                "User-Agent"
-            ] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:95.0) Gecko/20100101 Firefox/95.0"
             opts = {
                 "socket_timeout": 3,
             }
             if config.get("proxy", section="youtube_dl"):
                 opts["proxy"] = config.get("proxy", section="youtube_dl")
-            with YoutubeDL(params=opts) as ydl:
+            with yt_dlp.YoutubeDL(params=opts) as ydl:
                 media_info = ydl.extract_info(url, download=False)
                 assert isinstance(media_info, dict)
                 assert isinstance(session, aiohttp.ClientSession)
@@ -1807,7 +1802,7 @@ async def tiktok_function(message, _, args):
                     input_image_blob = io.BytesIO(await resp.read())
                 file_name = f'{media_info["id"]}.{media_info["formats"][0]["ext"]}'
         except yt_dlp.DownloadError:
-            with YoutubeDL(params=opts) as ydl:
+            with yt_dlp.YoutubeDL(params=opts) as ydl:
                 media_info = ydl.extract_info(url, download=False)
                 assert isinstance(media_info, dict)
                 assert isinstance(session, aiohttp.ClientSession)
