96fb3e60ba1bfa39272c43395d6cdcdfbcb742bf
Novalinium <nova@noblejury.com>
2022-11-01 02:06:57 -0400

    Escape percent

--- swag.py
+++ swag.py
@@ -3442,7 +3442,7 @@ async def glowfic_post_search(subj_content, exact=False, username=None, password
     global conn
     cur = conn.cursor()
     cur.execute(
-        "SELECT id, content FROM glowfic_posts WHERE content LIKE LIKE '%%s%';",
+        "SELECT id, content FROM glowfic_posts WHERE content LIKE '%%%s%%';",
         [subj_content],
     )
     quote = cur.fetchone()
