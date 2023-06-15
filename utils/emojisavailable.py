3599f77a874db1479ca657da166ad59dc084ec60
Novalinium <nova@noblejury.com>
2023-06-15 19:39:30 -0400

    Update CSS for emoji page

--- utils/emojisavailable.py
+++ utils/emojisavailable.py
@@ -4,7 +4,7 @@ import discord
 import os
 import sys
 
-FLETCHER_CONFIG = os.getenv('FLETCHER_CONFIG', './.fletcherrc')
+FLETCHER_CONFIG = os.getenv("FLETCHER_CONFIG", "./.fletcherrc")
 
 config = configparser.ConfigParser()
 config.read(FLETCHER_CONFIG)
@@ -14,10 +14,23 @@ intents.members = True
 client = discord.Client(intents=intents, chunk_guilds_at_startup=False)
 
 # token from https://discordapp.com/developers
-token = config['discord']['botToken']
+token = config["discord"]["botToken"]
+
 
 @client.event
 async def on_ready():
-    print("<ul>"+"".join([f"<li><img src='{emoji.url_as()}' height='2em' width='2em' loading='lazy' />{emoji.name}</li>" for emoji in client.emojis if emoji.is_usable()])+"</ul>")
+    print(
+        "<ul>"
+        + "".join(
+            [
+                f"<li><img src='{emoji.url_as()}' style='height:2em; width:2em' loading='lazy' />{emoji.name}</li>"
+                for emoji in client.emojis
+                if emoji.is_usable()
+            ]
+        )
+        + "</ul>"
+    )
     await client.close()
+
+
 client.run(token)
